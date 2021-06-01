5a0042a80f72d10487e6074d20edeba87b4faca5
lizsong88 <lizsong@stanford.edu>
2021-05-31 22:54:35 -0700

    added a few reappraisals

--- imgReapp.js
+++ imgReapp.js
@@ -1,4 +1,12 @@
 var reapp = '[ \
-    { "img": "img/blue.png", "reappraisal": "this is a blue circle" }, \
-    { "img": "img/orange.png", "reappraisal": "this is an orange circle" } \
+    { "img": "img/1270.png", "reappraisal": "Since the bug looks like it under a microscope, the bug is probably not as big in real life" }, \
+    { "img": "img/1271.png", "reappraisal": "The bugs look like they are happy and healthy in their enclosure" }, \
+    { "img": "img/1280.png", "reappraisal": "The mouse itself looks relatively unharmed despite the surroundings" }, \
+    { "img": "img/1301.png", "reappraisal": "The dog is excited and salivating to eat a piece of food being thrown to it" }, \
+    { "img": "img/2120.png", "reappraisal": "The man may be in the middle of talking about something he is passionate about" }, \
+    { "img": "img/2278.png", "reappraisal": "The girls may be looking up at the sky and cloudwatching" }, \
+    { "img": "img/2312.png", "reappraisal": "The boy is sad because he did not want to leave the park and his mom is consoling him" }, \
+    { "img": "img/2399.png", "reappraisal": "The woman is having a hard time deciding what to eat for dinner" } \
 ]';
+
+// template: { "img": "img/1271.png", "reappraisal": "" }, \
