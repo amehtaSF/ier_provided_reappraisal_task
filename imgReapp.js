53ec4102b9e79b2793c7d6fab245ce732d9e7af0
lizsong88 <lizsong@stanford.edu>
2021-06-12 23:58:22 -0700

    additional task modifications

--- imgReapp.js
+++ imgReapp.js
@@ -1,12 +1,29 @@
-var reapp = '[ \
-    { "img": "img/1270.png", "reappraisal": "Since the bug looks like it under a microscope, the bug is probably not as big in real life" }, \
-    { "img": "img/1271.png", "reappraisal": "The bugs look like they are happy and healthy in their enclosure" }, \
-    { "img": "img/1280.png", "reappraisal": "The mouse itself looks relatively unharmed despite the surroundings" }, \
-    { "img": "img/1301.png", "reappraisal": "The dog is excited and salivating to eat a piece of food being thrown to it" }, \
-    { "img": "img/2120.png", "reappraisal": "The man may be in the middle of talking about something he is passionate about" }, \
-    { "img": "img/2278.png", "reappraisal": "The girls may be looking up at the sky and cloudwatching" }, \
-    { "img": "img/2312.png", "reappraisal": "The boy is sad because he did not want to leave the park and his mom is consoling him" }, \
-    { "img": "img/2399.png", "reappraisal": "The woman is having a hard time deciding what to eat for dinner" } \
+/*
+	Reappraisal Images and Reappraisals 
+
+	Liz Song, <lizsong@stanford.edu>
+	May, 2021
+*/
+
+var reapp_low = '[ \
+    { "img": "img/1270.jpg", "reappraisal": "Since the bug looks like it under a microscope, the bug is probably not as big in real life" }, \
+    { "img": "img/1271.jpg", "reappraisal": "The bugs look like they are happy and healthy in their enclosure" }, \
+    { "img": "img/1280.jpg", "reappraisal": "The mouse itself looks relatively unharmed despite the surroundings" }, \
+    { "img": "img/1301.jpg", "reappraisal": "The dog is excited and salivating to eat a piece of food being thrown to it" }, \
+    { "img": "img/2120.jpg", "reappraisal": "The man may be in the middle of talking about something he is passionate about" }, \
+    { "img": "img/2278.jpg", "reappraisal": "The girls may be looking up at the sky and cloudwatching" }, \
+    { "img": "img/2312.jpg", "reappraisal": "The boy is sad because he did not want to leave the park and his mom is consoling him" }, \
+    { "img": "img/2399.jpg", "reappraisal": "The woman is having a hard time deciding what to eat for dinner" } \
 ]';
 
-// template: { "img": "img/1271.png", "reappraisal": "" }, \
+
+var reapp_high = '[ \
+    { "img": "img/blue.png", "reappraisal": "this is a blue circle" }, \
+    { "img": "img/orange.png", "reappraisal": "this is an orange circle" } \
+]';
+
+/*
+	template: { "img": "img/1271.jpg", "reappraisal": "" }, \
+
+	reminder: don't forget to remove comma after the last image in each array
+*/
